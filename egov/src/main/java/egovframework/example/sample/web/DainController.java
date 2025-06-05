6a00ea2ea0f3560ac71b63588db6054d64e9214f
Lee-jeong-wook <b01024134020@gmail.com>
2025-06-05 20:41:49 +0900

    Rest Controller 삭제

--- egov/src/main/java/egovframework/example/sample/web/DainController.java
+++ egov/src/main/java/egovframework/example/sample/web/DainController.java
@@ -1,14 +1,6 @@
 package egovframework.example.sample.web;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
-public class dainController {
+public class DainController {
 	
 	@Autowired
 	private DainService dainService;
@@ -41,5 +33,27 @@ public class dainController {
 	@GetMapping(value = "/signup.do")
 	public String signinPage() throws Exception{
 		return "sample/signup";
+	}
+
+	@PostMapping(value = "/updateComputer.do", consumes = {"application/json", "application/json;charset=UTF-8"})
+	public String updateComputer(@RequestBody MemberVO vo, RedirectAttributes redirectAttributes) {
+		dainService.updateComputer(vo.getId(), vo.getComputer());
+		redirectAttributes.addAttribute("id", vo.getId());
+		return "redirect:/room.do";
+	}
+
+	@RequestMapping(value = "/getUser.do")
+	public String login(@RequestParam String id, @RequestParam String pw, RedirectAttributes redirectAttributes) {
+		MemberVO member = dainService.getMemberInfo(id, pw);
+		redirectAttributes.addAttribute("id", member.getId());
+		return "redirect:/room.do";
+	}
+
+	@RequestMapping(value = "/login.do")
+	public String login(@RequestBody MemberVO vo, RedirectAttributes redirectAttributes) {
+		dainService.updateComputer(vo.getId(), vo.getComputer());
+		MemberVO member = dainService.getMember(vo.getId());
+		redirectAttributes.addAttribute("id", member.getId());
+		return "redirect:/room.do";
 	}
 }
